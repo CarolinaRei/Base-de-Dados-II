f1e0a9c33f2cb6c970a700eb70a585812ace0765
NSerus <nunoworkingserus@gmail.com>
2021-05-25 11:47:23 +0100

    Updates on EMP procedure

--- Procedure_EMP.sql
+++ Procedure_EMP.sql
@@ -13,6 +13,8 @@ create or replace procedure procedure_employees is
     
     sal_min_value NUMBER(4):= 1;
     sal_max_value NUMBER(6):= 2;
+    sal_min NUMBER(4):= 1;
+    sal_max NUMBER(6):= 2;
     
     sal NUMBER(4):= 1;
     commissiom NUMBER(5,3):= 2;
@@ -20,16 +22,27 @@ create or replace procedure procedure_employees is
     sal_pct NUMBER(3,2):=1.00;
     com_pct NUMBER(3,2):=1.00;
 begin
-    select min(commission_pct), max(commission_pct), min(salary), max(salary), avg(salary)
-    into com_min_value, com_max_value, sal_min_value, sal_max_value, avg_salary
+    select min(salary), max(salary)
+    into  sal_min_value, sal_max_value
     from employees;
     
-    insert into mini_dw_employees(id, salary_min, salary_max, salary_level)
-    select mini_emp_seq.nextval, sal_pt, com_pct;
+    sal_min := sal_min_value-100;
     
-    insert into dw_employees(id, employee_idk employees_mini_id, first_name, last_name, manager_id, hire_date, phone_number, email, salary)
+    for lvl in 1..7 loop
+        sal_max:= sal_min + 3000
+        
+        insert into mini_dw_employees(id, salary_min, salary_max, salary_level)
+        select mini_emp_seq.nextval, sal_min, sal_max, lvl
+        from employees;
+        sal_min := sal_min + 3000
+    end loop;
+    
+    
+    
+    insert into dw_employees(id, employee_id, employees_mini_id, first_name, last_name, manager_id, hire_date, phone_number, email, salary)
     select emp_seq.nextval, employee_id, id, first_name, last_name, manager_id, hire_date, phone_number, email, salary
-    from employees, mini_dw_employees;
+    from employees, mini_dw_employees
+    where salary > sal_min AND salary < sal_max;
 end;
 /
 
